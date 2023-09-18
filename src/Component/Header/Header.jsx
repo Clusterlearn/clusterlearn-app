133aa30b072d923fb76dfae046e71d6f9fa7f3fb
akinlope <akinrimisitt@gmail.com>
2023-09-18 05:14:39 -0500

    [Hamburger fixed]

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -56,8 +56,8 @@ const Header = () => {
   return (
     <>
       {/*flex justify-center items-center   items-center md:justify-between py-5 text-sm md:text-lg */}
-      <div className="relative flex flex-row items-center justify-between py-5 xl:px-10 sm:px-4 text-sm md:text-lg">
-        <div className="flex flex-row items-center gap-5 ">
+      <div className="relative flex md:flex-row items-center md:justify-between py-5 xl:px-10 sm:px-4 text-sm md:text-lg">
+        <div className="flex flex-row items-center gap-14">
           {!showMenu && (
             <>
               <Logo Text={"CLUSTER"} />
@@ -71,44 +71,48 @@ const Header = () => {
           )}
 
           {/* Hamburger Icon */}
-          <div className="-mt-5 md:hidden cursor-pointer block text-4xl">
+          <div className=" -mt-5 md:hidden cursor-pointer block text-4xl">
             <button
-              onClick={() => setShowMenu(!showMenu)}
-              className=" text- text-3xl hover:text-gray-200 duration-400 focus:outline-none "
+              onClick={() => {
+                console.log("clicked");
+                setShowMenu(!showMenu);
+              }}
+              className=" text-3xl hover:text-gray-200 duration-400 focus:outline-none "
             >
               {!showMenu ? (
                 <HamburgerNav />
               ) : (
-                <div className="w-80 float-right">
-                  <svg
-                    className=" mt-2 float-right "
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    fill="none"
-                  >
-                    <g clip-path="url(#clip0_626_2711)">
-                      <path
-                        d="M20.9174 11.3488C21.4683 10.8353 21.4987 9.97244 20.9852 9.42151C20.4717 8.87058 19.6088 8.84022 19.0579 9.3537L15.0677 13.0726L11.3488 9.08248C10.8353 8.53155 9.97244 8.50119 9.42151 9.01467C8.87058 9.52815 8.84022 10.391 9.3537 10.9419L13.0727 14.9321L9.08248 18.6511C8.53155 19.1646 8.50119 20.0274 9.01467 20.5784C9.52815 21.1293 10.391 21.1596 10.9419 20.6462L14.9321 16.9272L18.6511 20.9174C19.1646 21.4683 20.0274 21.4987 20.5784 20.9852C21.1293 20.4717 21.1596 19.6088 20.6462 19.0579L16.9272 15.0677L20.9174 11.3488Z"
-                        fill="#0F172A"
-                        fill-opacity="0.7"
-                      />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0 15C0 6.71573 6.71573 0 15 0C23.2843 0 30 6.71573 30 15C30 23.2843 23.2843 30 15 30C6.71573 30 0 23.2843 0 15ZM15 27.2727C8.22196 27.2727 2.72727 21.778 2.72727 15C2.72727 8.22196 8.22196 2.72727 15 2.72727C21.778 2.72727 27.2727 8.22196 27.2727 15C27.2727 21.778 21.778 27.2727 15 27.2727Z"
-                        fill="#0F172A"
-                        fill-opacity="0.7"
-                      />
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_626_2711">
-                        <rect width="30" height="30" fill="white" />
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </div>
+                // <div>Hello</div>
+                <div className=" mt-10 w-[364px] flex justify-end">
+                    <svg
+                      className=""
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      viewBox="0 0 30 30"
+                      fill="none"
+                    >
+                      <g clip-path="url(#clip0_626_2711)">
+                        <path
+                          d="M20.9174 11.3488C21.4683 10.8353 21.4987 9.97244 20.9852 9.42151C20.4717 8.87058 19.6088 8.84022 19.0579 9.3537L15.0677 13.0726L11.3488 9.08248C10.8353 8.53155 9.97244 8.50119 9.42151 9.01467C8.87058 9.52815 8.84022 10.391 9.3537 10.9419L13.0727 14.9321L9.08248 18.6511C8.53155 19.1646 8.50119 20.0274 9.01467 20.5784C9.52815 21.1293 10.391 21.1596 10.9419 20.6462L14.9321 16.9272L18.6511 20.9174C19.1646 21.4683 20.0274 21.4987 20.5784 20.9852C21.1293 20.4717 21.1596 19.6088 20.6462 19.0579L16.9272 15.0677L20.9174 11.3488Z"
+                          fill="#0F172A"
+                          fill-opacity="0.7"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          clip-rule="evenodd"
+                          d="M0 15C0 6.71573 6.71573 0 15 0C23.2843 0 30 6.71573 30 15C30 23.2843 23.2843 30 15 30C6.71573 30 0 23.2843 0 15ZM15 27.2727C8.22196 27.2727 2.72727 21.778 2.72727 15C2.72727 8.22196 8.22196 2.72727 15 2.72727C21.778 2.72727 27.2727 8.22196 27.2727 15C27.2727 21.778 21.778 27.2727 15 27.2727Z"
+                          fill="#0F172A"
+                          fill-opacity="0.7"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_626_2711">
+                          <rect width="30" height="30" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
               )}
             </button>
           </div>
